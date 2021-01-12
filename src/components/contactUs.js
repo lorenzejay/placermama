0c3ba691b0d576bab48366c8fc6903676f0e354b
Lorenze Jay <Lorenzejay@gmail.com>
2021-01-12 12:45:07 -0800

    used styled components for inputs

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -1,12 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+
+export const Form = styled.form`
+  input,
+  textarea {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
+    border-radius: 4px;
+  }
+`
 
 const ContactUs = () => {
   return (
-    <form className="flex flex-col justify-center items-center my-12 w-3/4 mx-auto overflow-visible">
-      <h1 className="uppercase text-2xl text-center">
+    <Form className="flex flex-col justify-center items-center my-12 w-3/4 mx-auto overflow-visible">
+      <h1 className="uppercase text-2xl text-center mb-12">
         We'd love to hear your story
       </h1>
-      <div className="mb-5 pt-0 w-full">
+      <div className="mb-5 pt-0 w-full ">
         <input
           type="text"
           placeholder="Name"
@@ -29,7 +38,7 @@ const ContactUs = () => {
           className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full rounded shadow-md"
         />
       </div>
-    </form>
+    </Form>
   )
 }
 
