9c1983736fd45abe1428e3575e7260d995deae30
Lorenze Jay <Lorenzejay@gmail.com>
2021-01-12 17:18:38 -0800

    ddeleted surrounding div from the inputs

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -6,6 +6,8 @@ export const Form = styled.form`
   textarea {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
     border-radius: 4px;
+    margin-bottom: 15px;
+    z-index: 20;
   }
 `
 
@@ -15,29 +17,32 @@ const ContactUs = () => {
       <h1 className="uppercase text-2xl text-center mb-12">
         We'd love to hear your story
       </h1>
-      <div className="mb-5 pt-0 w-full ">
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full "
-        />
-      </div>
-      <div className="mb-5 pt-0 w-full">
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full "
-        />
-      </div>
-      <div className="mb-5 pt-0 w-full">
-        <textarea
-          rows="5"
-          placeholder="Tell us your story"
-          className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full "
-        />
-      </div>
+
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full "
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full "
+      />
+
+      <textarea
+        rows="5"
+        placeholder="Tell us your story"
+        className="px-3 py-3 placeholder-gray-800 text-gray-700 relative bg-white text-sm outline-none focus:outline-none  w-full "
+      />
+      <button
+        className="bg-gray-700 w-full rounded text-white py-1 uppercase"
+        type="submit"
+      >
+        Submit
+      </button>
     </Form>
   )
 }
