6206f3c9be2bb1d51c228e29851aeeec4287ba02
Lorenze Jay <Lorenzejay@gmail.com>
2021-01-17 10:45:22 -0800

    commented out the insta query

--- src/components/instagrid.js
+++ src/components/instagrid.js
@@ -1,46 +1,46 @@
-import { graphql, useStaticQuery } from "gatsby"
-import React from "react"
+// import { graphql, useStaticQuery } from "gatsby"
+// import React from "react"
 
-const Instagrid = () => {
-  const data = useStaticQuery(graphql`
-    query Insta {
-      allInstaNode(limit: 9) {
-        edges {
-          node {
-            localFile {
-              childImageSharp {
-                fluid {
-                  src
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-  // console.log(data.allInstaNode.edges)
-  return (
-    <div className="grid grid-cols-3 gap-3 p-5">
-      {data.allInstaNode.edges.map((item, i) => {
-        console.log(item)
-        return (
-          <a
-            href="https://www.instagram.com/jmsd_films/"
-            target="_blank"
-            rel="noreferrer"
-            key={i}
-          >
-            <img
-              src={item.node.localFile.childImageSharp.fluid.src}
-              className="w-24 h-24 md:w-32 md:h-32 object-cover"
-              alt="instragram posts"
-            />
-          </a>
-        )
-      })}
-    </div>
-  )
-}
+// const Instagrid = () => {
+//   const data = useStaticQuery(graphql`
+//     query Insta {
+//       allInstaNode(limit: 9) {
+//         edges {
+//           node {
+//             localFile {
+//               childImageSharp {
+//                 fluid {
+//                   src
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   // console.log(data.allInstaNode.edges)
+//   return (
+//     <div className="grid grid-cols-3 gap-3 p-5">
+//       {data.allInstaNode.edges.map((item, i) => {
+//         console.log(item)
+//         return (
+//           <a
+//             href="https://www.instagram.com/jmsd_films/"
+//             target="_blank"
+//             rel="noreferrer"
+//             key={i}
+//           >
+//             <img
+//               src={item.node.localFile.childImageSharp.fluid.src}
+//               className="w-24 h-24 md:w-32 md:h-32 object-cover"
+//               alt="instragram posts"
+//             />
+//           </a>
+//         )
+//       })}
+//     </div>
+//   )
+// }
 
-export default Instagrid
+// export default Instagrid
