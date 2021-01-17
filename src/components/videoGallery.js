1d6a04cca1f872cfb9dbbc026065fd70f1121e4f
Lorenze Jay <Lorenzejay@gmail.com>
2021-01-17 15:56:08 -0800

    alignment issues with video fix?

--- src/components/videoGallery.js
+++ src/components/videoGallery.js
@@ -3,9 +3,9 @@ import Video from "./video"
 
 const VideoGallery = () => {
   return (
-    <div className="w-full  ">
+    <div className="w-full p-4">
       <h2 className="text-4xl mb-20 text-center">Movies</h2>
-      <div className="flex flex-col items-center justify-center my-24 p-4 xl:px-48 lg:grid lg:grid-cols-2 lg:gap-5 mx-auto">
+      <div className="mt-2 flex flex-col items-center justify-center sm:grid sm:grid-cols-2 gap-4 mx-auto">
         <Video
           videoSrc="https://player.vimeo.com/video/380106142?title=0&byline=0&portrait=0"
           title="Anna + Brian"
